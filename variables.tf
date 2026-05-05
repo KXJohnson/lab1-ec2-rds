11e3436c61f6ff423380398ded08c8dd61e37da3
Native_Sun <140997376+KXJohnson@users.noreply.github.com>
2026-05-05 01:46:06 -0400

    Define variables for project configuration
    Add variables for project configuration including database identifiers and common tags.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
+variable "project_name" {
+  type    = string
+  default = "lab-rds-app"
+}
 
+variable "db_identifier" {
+  type    = string
+  default = "lab-rds-app"
+}
+
+variable "db_name" {
+  type    = string
+  default = "notes"
+}
+
+variable "secret_path" {
+  type    = string
+  default = "lab/rds/mysql"
+}
+
+variable "common_tags" {
+  type = map(string)
+
+  default = {
+    Name        = "lab-rds-app"
+    Environment = "lab"
+    Project     = "Lab1"
+  }
+}
